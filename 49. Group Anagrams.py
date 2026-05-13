95ec27f85a7b9065fe4cbd247fb806dd0170f533
thilyhoan0603-dau <thilyhoan0603@gmail.com>
2026-05-14 01:28:31 +0700

    Update 49. Group Anagrams.py

---
+++ 49. Group Anagrams.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def groupAnagrams(self, strs):
+        
+        groups = {}
+        
+        for s in strs:
+            sorted_s = "".join(sorted(s))
+            
+            
+            if sorted_s not in groups:
+                groups[sorted_s] = []
+            
+            
+            groups[sorted_s].append(s)
+            
+
+        return list(groups.values())
