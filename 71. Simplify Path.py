dbf5e634677cc8326bcc75b00ec0e956a2027d41
thilyhoan0603-dau <thilyhoan0603@gmail.com>
2026-05-15 12:55:07 +0700

    Update 71. Simplify Path.py

---
+++ 71. Simplify Path.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def simplifyPath(self, path):
+        # Bước 1: Tách chuỗi theo dấu '/'
+        # split('/') sẽ tạo ra danh sách các tên thư mục, '.' và '..'
+        components = path.split('/')
+        stack = []
+        
+        for part in components:
+            # Nếu gặp '..', quay lại thư mục cha (xóa phần tử cuối stack)
+            if part == "..":
+                if stack:
+                    stack.pop()
+            # Bỏ qua các trường hợp không cần thiết: 
+            # chuỗi rỗng (do //), dấu chấm đơn '.'
+            elif part == "." or part == "":
+                continue
+            # Các trường hợp còn lại là tên thư mục hợp lệ
+            else:
+                stack.append(part)
+        
+        # Bước 2: Nối lại thành đường dẫn chuẩn
+        # Luôn bắt đầu bằng '/' và các thư mục cách nhau bởi '/'
+        return "/" + "/".join(stack)
